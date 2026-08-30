7f158bbf50c62c80639111f4f7e42fbe6204cb18
gherdaria33 <gher.daria@gmail.com>
2026-08-30 21:28:22 +0300

    1

--- podcasts-backend/express-backend/routes/index.js
+++ podcasts-backend/express-backend/routes/index.js
@@ -1,43 +1,16 @@
 const express = require("express");
 const authController = require("../controllers/authController");
 const trackController = require("../controllers/trackController");
-const authenticate = require("../middleware/auth");
+const authenticate = require("../middleware/authMiddleware");
+
 const router = express.Router();
-/*
- * Авторизация
- */
-router.post(
-  "/register",
-  authController.register,
-);
-router.post(
-  "/login",
-  authController.login,
-);
-/*
- * Треки
- */
-router.get(
-  "/tracks",
-  authenticate,
-  trackController.getTracks,
-);
-/*
- * Избранное
- */
-router.get(
-  "/favorites",
-  authenticate,
-  trackController.getFavorites,
-);
-router.post(
-  "/favorites",
-  authenticate,
-  trackController.addToFavorites,
-);
-router.delete(
-  "/favorites",
-  authenticate,
-  trackController.removeFromFavorites,
-);
+
+router.post("/register", authController.register);
+router.post("/login", authController.login);
+
+router.get("/tracks", trackController.getTracks);
+router.post("/favorites", authenticate, trackController.addToFavorites);
+router.delete("/favorites", authenticate, trackController.removeFromFavorites);
+router.get("/favorites", authenticate, trackController.getFavorites);
+
 module.exports = router;
