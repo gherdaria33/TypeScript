a335fb3f4972193a31586d63f13ae5a95e62cb3a
gherdaria33 <gher.daria@gmail.com>
2026-08-31 14:33:53 +0300

    1

--- frontend/src/services/trackService.ts
+++
@@ -1,9 +0,0 @@
-import { api } from './api';
-import type { Track } from '../types';
-
-export const trackService = {
-  async getTracks(): Promise<Track[]> {
-    // In the supplied backend /tracks is public.
-    return api.get<Track[]>('/tracks');
-  }
-};
